e145fe0a5508e9ba43854b79a3b9fcb3f09cab8a
Ajith Meleppat <87268944+ajithmeleppat@users.noreply.github.com>
2026-04-30 14:32:16 +0500

    Modify GUI
    Added running score, result and game count

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3)
-    console.log(computerChoice)
     if (computerChoice == 1){
         return "rock";
     }else if (computerChoice == 2){
@@ -10,64 +9,74 @@ function getComputerChoice(){
         return "scissors";
 }
 
-function getHumanChoice(){
-    let humanchoice = prompt("Enter your choice Rock, Paper or Scissors")
-    return humanchoice.toLowerCase()
-}
-
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice){
+    while(gameCount<=5){
         if (humanChoice === computerChoice){
-            return `Draw!!! Both chose ${humanChoice}`;
+            return 'Draw';
         }
         
-        if (humanChoice === 'rock'){
-            if (computerChoice === 'paper'){
-                computerScore = computerScore + 1;
-                return 'You lose!! Paper beats Rock';
-            }else {
-                humanScore = humanScore + 1;
-                return 'You win!! Rock beats scissors';
-            }
-        } else if (humanChoice === 'paper'){
-            if (computerChoice === 'scissors'){
-                computerScore = computerScore + 1;
-                return 'You lose!! Scissors beat Paper';
-            } else {
-                humanScore = humanScore + 1;
-                return 'You win!! Paper beats Rock';
-            }
-        } else{
-            if (computerChoice === 'rock'){
-                computerScore = computerScore + 1;
-                return 'You lose!! Rock beats Scissors'
-            } else {
-                humanScore = humanScore + 1;
-                return 'You win!! Rock beats Paper';
-            }
-        } 
+        if (humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'scissors' && computerChoice === 'paper' ||
+            humanChoice === 'paper' && computerChoice === 'rock'
+        ){
+            humanScore++;
+            return "You Win";
+        }else{ 
+            computerScore++;
+            return "You Lose";
+        }
     }
 
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    if (gameCount === 5){
+        alert("You win");
+        humanScore = 0;
+        computerScore = 0;
+        result = "";
+        gameCount = 0;
+        modifyDisplay("","")
 
-    console.log('Final score');
-    console.log(`You: ${humanScore}`);
-    console.log(`Computer: ${computerScore}`); 
-
-    if (humanScore === computerScore){
-        console.log('The game is a draw');
-    } else if (humanScore > computerScore){
-        console.log('You win!!');
-    } else{
-        console.log('You lose!!');
     }
 }
 
-playGame();
+function modifyDisplay(humanChoice, computerChoice){
+    const resultElement = document.querySelector("#result");
+    const humanChoiceElement = document.querySelector("#human_choice");
+    const computerChoiceElement = document.querySelector("#computer_choice");
+    const humanScoreElement = document.querySelector("#human_score");
+    const computerScoreElement = document.querySelector("#computer_score");
+    const gameCountElement = document.querySelector("#game-count");
+
+    resultElement.textContent = result;
+    humanChoiceElement.textContent = humanChoice;
+    computerChoiceElement.textContent = computerChoice;
+    humanScoreElement.textContent = humanScore;
+    computerScoreElement.textContent = computerScore;
+    gameCountElement.textContent = gameCount;
+
+
+}
+
+const buttons = document.querySelectorAll(".btn-choice");
+buttons.forEach(button => {
+    button.addEventListener('click',(e) => {
+        gameCount ++;
+        let humanChoice = e.target.id.slice(4);
+        let computerChoice = getComputerChoice();
+        result = playRound(humanChoice,computerChoice);
+        modifyDisplay(humanChoice, computerChoice);
+    })
+})
+
+const btn = document.querySelector("#btn-reset");
+btn.addEventListener('click',() => {
+    gameCount = 0;
+    result = "";
+    humanScore = 0;
+    computerScore = 0;
+    modifyDisplay("","");
+})
+
+let gameCount = 0;
+let humanScore = 0;
+let computerScore = 0;
+let result = "";
